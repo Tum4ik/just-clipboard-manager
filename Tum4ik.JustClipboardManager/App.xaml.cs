554ac2280795b1a21f5f627cd0f331242313f51a
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2022-07-17 22:32:20 +0300

    Basic configuration.

--- Tum4ik.JustClipboardManager/App.xaml.cs
+++ Tum4ik.JustClipboardManager/App.xaml.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Linq;
+using System.Reflection;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Tum4ik.JustClipboardManager.Views;
 
 namespace Tum4ik.JustClipboardManager;
@@ -15,5 +19,41 @@ public partial class App : Application
     var app = new App();
     var tray = new TrayIcon();
     app.Run();
+  }
+
+
+  public IConfiguration Configuration { get; }
+  public IServiceProvider ServiceProvider { get; }
+
+
+  public App()
+  {
+    Configuration = ConfigureAppConfiguration();
+    ServiceProvider = ConfigureServices(Configuration);
+  }
+
+
+  private IConfiguration ConfigureAppConfiguration()
+  {
+    var assembly = Assembly.GetExecutingAssembly();
+    var appsettingsResourceName = assembly.GetManifestResourceNames().Single(n => n.EndsWith("appsettings.json"));
+    // Don't use "using" keyword for appsettingsStream here - it will break the settings reading process.
+    // The stream will be disposed by StreamReader internally anyway.
+    var appsettingsStream = assembly.GetManifestResourceStream(appsettingsResourceName);
+    return new ConfigurationBuilder()
+      .SetBasePath(AppContext.BaseDirectory)
+      .AddJsonStream(appsettingsStream)
+      .Build();
+  }
+
+
+  private IServiceProvider ConfigureServices(IConfiguration configuration)
+  {
+    var services = new ServiceCollection();
+
+    services
+      .AddSingleton(configuration);
+
+    return services.BuildServiceProvider();
   }
 }
