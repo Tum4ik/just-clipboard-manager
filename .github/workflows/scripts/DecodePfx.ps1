df88775ed72da78f7cd894907768abc060927659
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2022-10-10 00:35:08 +0300

    extracting powershell to separate files

---
+++ .github/workflows/scripts/DecodePfx.ps1
@@ -0,0 +1,14 @@
+param (
+  [string] $Base64Pfx,
+  [string] $OutputPath
+)
+
+if (!$Base64Pfx) {
+  throw "The Base64 encoded pfx data is missing. Use -Base64Pfx parameter."
+}
+if (!$OutputPath) {
+  throw "The output file path is missing. Use -OutputPath parameter."
+}
+
+$pfxCertBytes = [System.Convert]::FromBase64String($Base64Pfx)
+[IO.File]::WriteAllBytes($OutputPath, $pfxCertBytes)
