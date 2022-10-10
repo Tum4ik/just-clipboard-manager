6451bfec1b0bb69b2e533ed631a06300b0a77cb2
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2022-10-10 14:22:19 +0300

    extracting powershell to separate files

--- .github/workflows/scripts/SignMyCodeFiles.ps1
+++ .github/workflows/scripts/SignMyCodeFiles.ps1
@@ -4,6 +4,19 @@ param (
   [string] $PfxPassword
 )
 
-$filesToSign = Get-ChildItem -Recurse -Include *.dll,*.exe $FilesDirectoryPath | Where { ! $_.PSIsContainer } | Select-Object -ExpandProperty FullName
-$signtool = "C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x86\signtool.exe"
+if (!$FilesDirectoryPath) {
+  throw "The directory to sign files is not specified. Use -FilesDirectoryPath parameter."
+}
+if (!$PfxFilePath) {
+  throw "The PFX file path is missing. Use -PfxFilePath parameter."
+}
+if (!$PfxPassword) {
+  throw "The PFX password is missing. Use -PfxPassword parameter."
+}
+
+$filesToSign = Get-ChildItem -Recurse -Include *.dll,*.exe $FilesDirectoryPath `
+  | Where { ! $_.PSIsContainer } `
+  | Select-Object -ExpandProperty FullName
+# 17763
+$signtool = "C:\Program Files (x86)\Windows Kits\10\bin\10.0.20348.0\x64\signtool.exe"
 & $signtool sign /f $PfxFilePath /p $PfxPassword $filesToSign
