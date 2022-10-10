dd147063a8fe35cb2e54c350f782b4bb29aca2e9
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2022-10-10 14:50:11 +0300

    sign only my files

--- .github/workflows/scripts/SignMyCodeFiles.ps1
+++ .github/workflows/scripts/SignMyCodeFiles.ps1
@@ -14,9 +14,11 @@ if (!$PfxPassword) {
   throw "The PFX password is missing. Use -PfxPassword parameter."
 }
 
-$filesToSign = Get-ChildItem -Recurse -Include *.dll,*.exe $FilesDirectoryPath `
-  | Where { ! $_.PSIsContainer } `
+$filesToSign = Get-ChildItem -Recurse `
+  -Include `
+    JustClipboardManager.dll, JustClipboardManager.exe, Tum4ik.Attributes.dll, Tum4ik.EventAggregator.dll, `
+    Tum4ik.JustClipboardManager.Icons.dll `
+  $FilesDirectoryPath `
   | Select-Object -ExpandProperty FullName
-# 17763
 $signtool = "C:\Program Files (x86)\Windows Kits\10\bin\10.0.20348.0\x64\signtool.exe"
 & $signtool sign /f $PfxFilePath /p $PfxPassword $filesToSign
