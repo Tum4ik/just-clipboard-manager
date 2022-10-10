f1a546bfb8041b71a108d5dbe83c531a556586df
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2022-10-10 14:13:31 +0300

    extracting powershell to separate files

---
+++ .github/workflows/scripts/SignMyCodeFiles.ps1
@@ -0,0 +1,9 @@
+param (
+  [string] $FilesDirectoryPath,
+  [string] $PfxFilePath,
+  [string] $PfxPassword
+)
+
+$filesToSign = Get-ChildItem -Recurse -Include *.dll,*.exe $FilesDirectoryPath | Where { ! $_.PSIsContainer } | Select-Object -ExpandProperty FullName
+$signtool = "C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x86\signtool.exe"
+& $signtool sign /f $PfxFilePath /p $PfxPassword $filesToSign
