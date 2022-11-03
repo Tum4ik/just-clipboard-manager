1b83721066844a0701a4b2f5a0868e454c797200
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2022-11-03 19:55:07 +0200

    EF bundle creation.

---
+++ .github/workflows/scripts/CreateMigrationsBundle.ps1
@@ -0,0 +1,27 @@
+param (
+  [string] $Architecture,
+  [string] $OutputDir
+)
+
+$outputFile = Join-Path $OutputDir $Architecture "efbundle.exe"
+$project = Join-Path "Tum4ik.JustClipboardManager" "Tum4ik.JustClipboardManager.csproj"
+$framework = "net6.0-windows"
+$configuration = "Release"
+if ($Architecture -eq "x64") {
+  $targetRuntime = "win-x64"
+}
+else if ($Architecture -eq "x86") {
+  $targetRuntime = "win-x86"
+}
+else {
+  throw "Unsupported target runtime."
+}
+
+dotnet tool install -g dotnet-ef
+dotnet ef migrations bundle -f --self-contained --no-build `
+  -o $outputFile `
+  -r $targetRuntime `
+  -p $project `
+  -s $project `
+  --framework $framework `
+  --configuration $configuration
