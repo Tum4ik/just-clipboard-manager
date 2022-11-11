c2effe45081ca0ad9eb70ba3193c8c4a809afe47
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2022-11-11 23:51:23 +0200

    app configuration

--- YTSoft.JustClipboardManager/App.xaml.cs
+++ YTSoft.JustClipboardManager/App.xaml.cs
@@ -1,4 +1,8 @@
+using System.Reflection;
 using Microsoft.AppCenter.Crashes;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
 using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml;
 using Microsoft.Windows.AppLifecycle;
@@ -8,7 +12,7 @@ namespace YTSoft.JustClipboardManager;
 /// <summary>
 /// Provides application-specific behavior to supplement the default Application class.
 /// </summary>
-public partial class App : Application
+public partial class App
 {
   [STAThread]
   public static async Task Main(string[] args)
@@ -43,17 +47,38 @@ public partial class App : Application
   }
 
 
+  private readonly IHost _host;
+
   public App()
   {
     InitializeComponent();
+    _host = Host.CreateDefaultBuilder()
+      .ConfigureAppConfiguration((context, builder) =>
+      {
+        builder.Properties.Clear();
+        builder.Sources.Clear();
+        var assembly = Assembly.GetExecutingAssembly();
+        var appsettingsResourceName = assembly
+          .GetManifestResourceNames()
+          .Single(n => n.EndsWith("appsettings.json", StringComparison.InvariantCultureIgnoreCase));
+        // Don't use "using" keyword for appsettingsStream here - it will break the settings reading process.
+        // The stream will be disposed by StreamReader internally anyway.
+        var appsettingsStream = assembly.GetManifestResourceStream(appsettingsResourceName)!;
+        builder.AddJsonStream(appsettingsStream);
+#if DEBUG
+        builder.AddUserSecrets(Assembly.GetExecutingAssembly());
+#endif
+      })
+      .ConfigureServices((context, services) =>
+      {
+        var msappc = context.Configuration["MicrosoftAppCenterSecret"];
+      })
+      .Build();
   }
 
 
   protected override void OnLaunched(LaunchActivatedEventArgs args)
   {
-    m_window = new MainWindow();
-    m_window.Activate();
+    base.OnLaunched(args);
   }
-
-  private Window m_window;
 }
