6ad7efc117158670930e972eb313a60084d9505f
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2022-11-13 22:27:37 +0200

    To .NET 7

--- Tum4ik.JustClipboardManager/Data/Models/FormattedDataObject.cs
+++ Tum4ik.JustClipboardManager/Data/Models/FormattedDataObject.cs
@@ -4,10 +4,10 @@ internal class FormattedDataObject
 {
   public int Id { get; set; }
 
-  public byte[] Data { get; set; } = null!;
-  public string DataDotnetType { get; set; } = null!;
-  public string Format { get; set; } = null!;
-  public int FormatOrder { get; set; }
+  public required byte[] Data { get; set; }
+  public required string DataDotnetType { get; set; }
+  public required string Format { get; set; }
+  public required int FormatOrder { get; set; }
 
   public virtual Clip Clip { get; set; } = null!;
 }
