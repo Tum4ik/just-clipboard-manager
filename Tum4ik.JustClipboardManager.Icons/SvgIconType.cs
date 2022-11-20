cdff47dc28bf970767dcb4106e5c4b6a17af19fd
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2022-11-20 23:57:57 +0200

    menu items checkability

--- Tum4ik.JustClipboardManager.Icons/SvgIconType.cs
+++ Tum4ik.JustClipboardManager.Icons/SvgIconType.cs
@@ -1,6 +1,7 @@
 namespace Tum4ik.JustClipboardManager.Icons;
 public enum SvgIconType
 {
+  [SvgIconResource("check")] Check,
   [SvgIconResource("chevron")] Chevron,
   [SvgIconResource("file")] File,
   [SvgIconResource("files")] Files
