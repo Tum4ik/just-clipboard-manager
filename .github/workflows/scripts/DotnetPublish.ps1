77802dcc2e266b2034cf866ee3a4e417b9fff772
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2023-03-29 21:58:51 +0300

    msbuild test 1

--- .github/workflows/scripts/DotnetPublish.ps1
+++ .github/workflows/scripts/DotnetPublish.ps1
@@ -3,10 +3,14 @@ param (
   [string] $Version
 )
 
-msbuild -restore /p:Configuration=Release /p:PublishProfile=FolderProfile_$Architecture
-dotnet publish --no-build -c Release `
+msbuild -restore `
+  /t:Build /t:Publish `
+  /p:Configuration=Release `
   /p:PublishProfile=FolderProfile_$Architecture `
   /p:Version=$Version
+#dotnet publish --no-build -c Release `
+#  /p:PublishProfile=FolderProfile_$Architecture `
+#  /p:Version=$Version
 
 if ($LastExitCode -ne 0) {
   throw
