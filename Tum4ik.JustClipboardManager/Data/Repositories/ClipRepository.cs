35c5dd44fde0d95329d4c9c5767a7bc818e5bc72
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2023-05-04 17:06:06 +0300

    back to previous sql call

--- Tum4ik.JustClipboardManager/Data/Repositories/ClipRepository.cs
+++ Tum4ik.JustClipboardManager/Data/Repositories/ClipRepository.cs
@@ -55,18 +55,11 @@ internal class ClipRepository : IClipRepository
     using var dbContext = await _dbContextFactory.CreateDbContextAsync().ConfigureAwait(false);
     var clipIdsToRemove = dbContext.Clips
       .Where(c => c.ClippedAt < date)
-      .Select(c => c.Id)
-      .ToImmutableArray();
-    var parameterNames = new string[clipIdsToRemove.Length];
-    var sqlParameters = new SqliteParameter[clipIdsToRemove.Length];
-    for (var i = 0; i < clipIdsToRemove.Length; i++)
-    {
-      var parameterName = $"@p{i}";
-      parameterNames[i] = parameterName;
-      sqlParameters[i] = new SqliteParameter(parameterName, clipIdsToRemove[i]);
-    }
-    var sql = $"DELETE FROM Clips WHERE Id IN ({string.Join(",", parameterNames)})";
-    await dbContext.Database.ExecuteSqlRawAsync(sql, sqlParameters).ConfigureAwait(false);
+      .Select(c => c.Id);
+    var clipIdsCommaSeparated = string.Join(",", clipIdsToRemove);
+    await dbContext.Database
+      .ExecuteSqlRawAsync($"DELETE FROM Clips WHERE Id IN ({clipIdsCommaSeparated})")
+      .ConfigureAwait(false);
   }
 
 
