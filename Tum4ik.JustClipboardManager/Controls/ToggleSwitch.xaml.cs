b98a089adc788efa65ce9a4a4689bd3a82ee8593
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2023-08-30 22:26:40 +0300

    ToggleSwitch control.

---
+++ Tum4ik.JustClipboardManager/Controls/ToggleSwitch.xaml.cs
@@ -0,0 +1,39 @@
+using System.Windows;
+using System.Windows.Input;
+
+namespace Tum4ik.JustClipboardManager.Controls;
+
+/// <summary>
+/// Interaction logic for ToggleSwitch.xaml
+/// </summary>
+public partial class ToggleSwitch
+{
+  public ToggleSwitch()
+  {
+    InitializeComponent();
+  }
+
+
+  public static readonly DependencyProperty IsKeyboardNavigatedFocusProperty = DependencyProperty.Register(
+    nameof(IsKeyboardNavigatedFocus), typeof(bool), typeof(ToggleSwitch)
+  );
+  public bool IsKeyboardNavigatedFocus
+  {
+    get => (bool) GetValue(IsKeyboardNavigatedFocusProperty);
+    set => SetValue(IsKeyboardNavigatedFocusProperty, value);
+  }
+
+
+  protected override void OnGotFocus(RoutedEventArgs e)
+  {
+    base.OnGotFocus(e);
+    IsKeyboardNavigatedFocus = InputManager.Current.MostRecentInputDevice is KeyboardDevice;
+  }
+
+
+  protected override void OnLostFocus(RoutedEventArgs e)
+  {
+    base.OnLostFocus(e);
+    IsKeyboardNavigatedFocus = false;
+  }
+}
