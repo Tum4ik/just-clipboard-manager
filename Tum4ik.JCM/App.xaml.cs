69f864cb37c7fcbd827925b02b8494d4a184ebb4
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2024-01-13 22:27:12 +0200

    global usings

--- Tum4ik.JCM/App.xaml.cs
+++ Tum4ik.JCM/App.xaml.cs
@@ -1,21 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
@@ -26,7 +8,7 @@ namespace Tum4ik.JCM;
 public partial class App : Application
 {
   /// <summary>
-  /// Initializes the singleton application object.  This is the first line of authored code
+  /// Initializes the singleton application object. This is the first line of authored code
   /// executed, and as such is the logical equivalent of main() or WinMain().
   /// </summary>
   public App()
---
+++ Tum4ik.JCM/Usings.cs
@@ -0,0 +1,17 @@
+global using System;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using System.Runtime.InteropServices.WindowsRuntime;
+global using Microsoft.UI.Xaml;
+global using Microsoft.UI.Xaml.Controls;
+global using Microsoft.UI.Xaml.Controls.Primitives;
+global using Microsoft.UI.Xaml.Data;
+global using Microsoft.UI.Xaml.Input;
+global using Microsoft.UI.Xaml.Media;
+global using Microsoft.UI.Xaml.Navigation;
+global using Microsoft.UI.Xaml.Shapes;
+global using Windows.ApplicationModel;
+global using Windows.ApplicationModel.Activation;
+global using Windows.Foundation;
+global using Windows.Foundation.Collections;
