81bbe6822c4f4f47555eb4b4720dc77e34cc53a4
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2024-03-15 21:35:12 +0200

    Disable paste window controls if settings mode is enabled.

---
+++ Tum4ik.JustClipboardManager/Converters/InvertedBoolConverter.cs
@@ -0,0 +1,19 @@
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Tum4ik.JustClipboardManager.Converters;
+
+[ValueConversion(typeof(bool), typeof(bool))]
+internal class InvertedBoolConverter : IValueConverter
+{
+  public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+  {
+    return !(bool) value;
+  }
+
+
+  public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+  {
+    return !(bool) value;
+  }
+}
