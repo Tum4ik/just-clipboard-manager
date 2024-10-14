1d051413e6256e0b02aacd0e6ae18e4fbde99a8a
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2024-10-14 23:53:18 +0300

    added electron

---
+++ main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  win.loadFile('dist/just-clipboard-manager/browser/index.html');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
