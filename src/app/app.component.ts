478a61d7011bc939ddc503948154002dad06412f
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2024-10-15 23:34:02 +0300

    electron using typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'just-clipboard-manager';
+  title = 'just clipboard manager';
 }
