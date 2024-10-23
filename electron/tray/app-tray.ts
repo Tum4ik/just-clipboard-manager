1678a910be272dec8cc32d2892f9a0c9946be2bb
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2024-10-23 23:26:47 +0300

    paste window test

--- electron/tray/app-tray.ts
+++ electron/tray/app-tray.ts
@@ -1,4 +1,4 @@
-import { App, BrowserWindow, Menu, Tray } from "electron";
+import { App, BrowserWindow, globalShortcut, Menu, NativeImage, nativeImage, Tray } from "electron";
 import path from 'path';
 
 export class AppTray {
@@ -19,17 +19,17 @@ export class AppTray {
     if (process.platform == 'linux') {
       trayIconFileName = app.isPackaged ? 'tray.png' : 'tray-dev.png';
     }
-    const trayIconFilePath = path.join(this.dirname, 'assets', process.platform, trayIconFileName);
-    this.tray = new Tray(trayIconFilePath);
+    const trayIcon = nativeImage.createFromPath(path.join(this.dirname, 'assets', process.platform, trayIconFileName));
+    this.tray = new Tray(trayIcon);
     this.tray.setToolTip('Just Clipboard Manager');
     this.tray.setContextMenu(Menu.buildFromTemplate([
       // todo: customize tray menu, see https://github.com/max-mapper/menubar
       {
         label: 'Settings',
-        click: this.showMainWindow.bind(this)
+        click: () => this.showMainWindow(trayIcon)
       },
       {
-        label: 'About',
+        label: 'About'
       },
       {
         type: 'separator'
@@ -53,21 +53,38 @@ export class AppTray {
         click: this.app.quit
       }
     ]));
+
+    globalShortcut.register('Ctrl+Shift+Q', this.showPasteWindow.bind(this));
   }
 
 
-  private showMainWindow() {
+  private showMainWindow(icon: NativeImage) {
     const win = new BrowserWindow({
       minWidth: 800,
       minHeight: 600,
       width: 1000,
       height: 750,
+      icon: icon,
       // titleBarStyle: 'hidden',
       webPreferences: {
         devTools: !this.app.isPackaged
       },
     });
     this.loadWindow(win, 'main-window');
+  }
+
+
+  private showPasteWindow() {
+    const win = new BrowserWindow({
+      width: 400,
+      height: 400,
+      titleBarStyle: 'hidden',
+      opacity: .5,
+      webPreferences: {
+        devTools: !this.app.isPackaged
+      }
+    });
+    this.loadWindow(win, 'paste-window');
   }
 
 
