c86559bbf2f0bfb12742a89ab679425692457734
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2024-11-22 00:17:26 +0200

    fixed parameters passing

--- scripts/prod-electron-forge-make.ps1
+++ scripts/prod-electron-forge-make.ps1
@@ -1,9 +1,9 @@
 npm run copy-files-from-to
-npm run tsc -p tsconfig.electron.json
+npm run tsc -- -p tsconfig.electron.json
 
 cd ./plugins/text-plugin
 npx rollup -c
 cd ../../
 
 npm run build
-npm run electron-forge make
+npm run electron-forge -- make
