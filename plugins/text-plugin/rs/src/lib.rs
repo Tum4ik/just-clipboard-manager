52703c0de4f86724521915c62afa65ed829e1964
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2025-01-14 20:14:28 +0200

    rust plugin basics

---
+++ plugins/text-plugin/rs/src/lib.rs
@@ -0,0 +1,4 @@
+#[no_mangle]
+pub fn test() -> u32 {
+  42
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,8 +4,31 @@ fn greet(name: &str) -> String {
   format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+use libloading::Error;
+use libloading::Library;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+  unsafe {
+    println!("=============== start unsafe");
+    let lib = Library::new("text_plugin.dll").expect("lib error");
+
+    // let test_func: libloading::Symbol<unsafe extern "C" fn() -> String> = lib.get(b"test");
+    let test_func: Result<libloading::Symbol<unsafe extern "C" fn() -> u32>, Error> =
+      lib.get(b"test");
+    match test_func {
+      Ok(func) => {
+        let str = func();
+        println!("{str}");
+      }
+      Err(e) => {
+        println!("=============== func error");
+        println!("{e}");
+        // GetProcAddress failed
+      }
+    }
+  }
+
   tauri::Builder::default()
     .plugin(tauri_plugin_opener::init())
     .plugin(tauri_plugin_fs::init())
