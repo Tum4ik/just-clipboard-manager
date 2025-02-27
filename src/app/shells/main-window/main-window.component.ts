99f8a71759389a69fd81479254126fb258eaaad8
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2025-02-27 19:16:46 +0200

    Add main and paste window capabilities with necessary permissions

--- src/app/shells/main-window/main-window.component.ts
+++ src/app/shells/main-window/main-window.component.ts
@@ -1,30 +1,20 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, ElementRef, Inject, Renderer2 } from '@angular/core';
-import { BaseDirectory, readFile, readDir } from '@tauri-apps/plugin-fs';
-import { ClipboardDataPlugin } from 'just-clipboard-manager-pdk';
+import { Component } from '@angular/core';
+import { Button } from 'primeng/button';
+import { Menubar } from 'primeng/menubar';
 
 @Component({
   selector: 'jcm-main-window',
-  imports: [],
   templateUrl: './main-window.component.html',
-  styleUrl: './main-window.component.scss'
+  styleUrl: './main-window.component.scss',
+  imports: [
+    Menubar,
+    Button
+  ]
 })
 export class MainWindowComponent {
   constructor(
-    private readonly renderer: Renderer2,
-    private readonly element: ElementRef,
-    @Inject(DOCUMENT) private readonly document: Document
+
   ) { }
 
-  async testPlugin() {
 
-    const pluginFileBytes = await readFile('plugins/text-plugin/plugin-bundle.mjs', {
-      baseDir: BaseDirectory.Resource
-    });
-    const blob = new Blob([pluginFileBytes], { type: 'application/javascript' });
-    const url = URL.createObjectURL(blob);
-    const pluginModule = await import(url);
-    const pluginInstance: ClipboardDataPlugin = pluginModule.pluginInstance;
-    // this.renderer.appendChild(this.element.nativeElement, pluginInstance.getRepresentationDataElement(Uint8Array.from([]), this.document));
-  }
 }
