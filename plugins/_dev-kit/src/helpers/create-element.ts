988cb9123f302e3ea5e3ee9e857af1e356da91a9
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2025-05-31 22:33:55 +0300

    Refactor createElement function to consolidate options parameter and improve readability

--- plugins/_dev-kit/src/helpers/create-element.ts
+++ plugins/_dev-kit/src/helpers/create-element.ts
@@ -1,12 +1,12 @@
 export function createElement<K extends keyof HTMLElementTagNameMap>(
   document: Document,
   tagName: K,
-  props?: HTMLElementTagNameMap[K] | Record<string, any>,
-  style?: CSSStyleDeclaration | Record<string, any>,
-  children?: Node[],
-  options?: ElementCreationOptions
+  options?: CreateElementOptions<K>
 ): HTMLElementTagNameMap[K] {
   const el = document.createElement(tagName, options);
+  const props = options?.props;
+  const style = options?.style;
+  const children = options?.children;
   if (props) {
     for (const key in props) {
       (el as any)[key] = (props as any)[key];
@@ -23,4 +23,10 @@ export function createElement<K extends keyof HTMLElementTagNameMap>(
     }
   }
   return el;
+}
+
+export interface CreateElementOptions<K extends keyof HTMLElementTagNameMap> extends ElementCreationOptions {
+  props?: HTMLElementTagNameMap[K] | Record<string, any>;
+  style?: CSSStyleDeclaration | Record<string, any>;
+  children?: Node[];
 }
