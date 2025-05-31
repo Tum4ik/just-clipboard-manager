4bf17dda84157b9df25b7f20adf1a1d3470ccebc
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2025-05-31 19:17:41 +0300

    Bump version to 4.0.23 and enhance createElement function to accept props

--- plugins/_dev-kit/src/helpers/create-element.ts
+++ plugins/_dev-kit/src/helpers/create-element.ts
@@ -1,14 +1,20 @@
 export function createElement<K extends keyof HTMLElementTagNameMap>(
   document: Document,
   tagName: K,
-  style?: CSSStyleDeclaration,
+  props?: HTMLElementTagNameMap[K] | Record<string, any>,
+  style?: CSSStyleDeclaration | Record<string, any>,
   children?: Node[],
   options?: ElementCreationOptions
 ): HTMLElementTagNameMap[K] {
   const el = document.createElement(tagName, options);
+  if (props) {
+    for (const key in props) {
+      (el as any)[key] = (props as any)[key];
+    }
+  }
   if (style) {
     for (const key in style) {
-      el.style[key] = style[key];
+      (el.style as any)[key] = (style as any)[key];
     }
   }
   if (children) {
