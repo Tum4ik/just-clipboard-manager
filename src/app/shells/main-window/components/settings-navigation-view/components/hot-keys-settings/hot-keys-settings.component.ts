08abfa612968c9f890db14c3f57c8199cc971709
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2025-09-08 23:52:15 +0300

    call paste window setting card

--- src/app/shells/main-window/components/settings-navigation-view/components/hot-keys-settings/hot-keys-settings.component.ts
+++ src/app/shells/main-window/components/settings-navigation-view/components/hot-keys-settings/hot-keys-settings.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { ScrollViewComponent } from "../../../scroll-view/scroll-view.component";
+import { SettingsCardComponent } from "../../../settings-card/settings-card.component";
 
 @Component({
   selector: 'jcm-hot-keys-settings',
-  imports: [],
   templateUrl: './hot-keys-settings.component.html',
-  styleUrl: './hot-keys-settings.component.scss'
+  styleUrl: './hot-keys-settings.component.scss',
+  imports: [
+    ScrollViewComponent,
+    SettingsCardComponent,
+    TranslatePipe,
+  ]
 })
 export class HotKeysSettingsComponent {
 
