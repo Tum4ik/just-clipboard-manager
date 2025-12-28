2313e1ae7f6739e63f542de983e81aad183f80fd
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2025-12-28 21:47:10 +0200

    Update dependencies and enhance GithubService to dynamically fetch plugins list

--- src/app/shells/main-window/services/github.service.ts
+++ src/app/shells/main-window/services/github.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Octokit } from "@octokit/rest";
+import { major } from 'semver';
+import packageJson from '../../../../../package.json';
 import { MonitoringService } from '../../../core/services/monitoring.service';
 
 @Injectable({ providedIn: 'root' })
@@ -10,10 +12,11 @@ export class GithubService {
 
   async getPluginsListAsBase64ContentAsync(): Promise<string | null> {
     try {
+      const pdkVersion = packageJson.dependencies['just-clipboard-manager-pdk'];
       const response = await this.octokit.repos.getContent({
         owner: 'Tum4ik',
         repo: 'just-clipboard-manager-plugins',
-        path: 'plugins-list-4.0.json',
+        path: `release/${major(pdkVersion)}/list.json`,
       });
       if (response.status !== 200) {
         this.monitoringService.warning(`Failed to fetch plugins list. Status code: ${response.status}`);
