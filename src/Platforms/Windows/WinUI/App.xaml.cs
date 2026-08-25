b5ba016ed9a341d0394546027c39089080c72ccc
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2026-08-25 23:35:26 +0300

    vs code launch config

--- src/Platforms/Windows/WinUI/App.xaml.cs
+++ src/Platforms/Windows/WinUI/App.xaml.cs
@@ -1,20 +1,5 @@
-﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -44,6 +29,13 @@ namespace JustClipboardManager
     protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
     {
       _window = new MainWindow();
+      // _window.AppWindow.TitleBar.ExtendsContentIntoTitleBar = true;
+      if (_window.AppWindow.Presenter is OverlappedPresenter presenter)
+      {
+        // presenter.IsMaximizable = false;
+        // presenter.IsMinimizable = false;
+        presenter.SetBorderAndTitleBar(true, false);
+      }
       _window.Activate();
     }
   }
